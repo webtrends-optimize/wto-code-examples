f65d4f295a32ae4a7075f15ee3632e67cd014be5
Sandeep Shah <sandeep89@gmail.com>
2024-10-03 11:47:08 +0100

    Create track-many-metrics.js

---
+++ tracking-metrics/track-many-metrics.js
@@ -0,0 +1,24 @@
+// Based on the page https://www.webtrends-optimize.com/google-optimize-sunset/
+
+let list = {
+  // 'metric name': 'selector'
+  
+  'Click_MobileNav_Launch': '#toggle-mobile-menu',
+  'Click_RequestDemo_All': 'a.launch-lightboxform',
+  // ...
+};
+let testAlias = "ta_something";
+
+document.body.addEventListener("click", e => {
+
+  for(let conversionPoint in list){
+    let selector = list[cp];
+    
+    if(e.target.closest(selector)){
+      // track metric 
+
+      WT.click({ testAlias, conversionPoint });
+    }
+  }
+  
+});
